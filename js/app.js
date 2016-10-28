f239cff02fffcc615da3e1f86089e34ee7853e03
yasuper8 <ysuper8@gmail.com>
2016-10-28 14:18:55 -0700

    {

--- js/app.js
+++ js/app.js
@@ -2,5 +2,23 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+console.log("app.js inked!")
+
+
+
+$('#a1').on('click', function() {
+  console.log($(this).text());
+  $(this).text("x");
+})
+
+
+
+
+
+
+
+
+
+
 
 });
