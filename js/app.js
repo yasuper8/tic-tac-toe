36ee511e9b3c955bb0b68c4870c341c07b803ca8
yasuper8 <ysuper8@gmail.com>
2016-10-28 15:46:15 -0700

    adds a feature to check whose turn and funciton putXorO

--- js/app.js
+++ js/app.js
@@ -5,16 +5,58 @@ $(document).ready(function() {
 console.log("app.js inked!")
 
 
+var resetGridArr = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
 
-$('#a1').on('click', function() {
+
+
+//keep track of player
+var xTurn = true;
+
+//chaecks whos turn and place X or O
+function putXorO(whosTurn) {
+  if (xTurn) {
+    xTurn = false;
+    $(this).text("X");
+    console.log("second log", whosTurn);
+  } else {
+    xTurn = true;
+    $(this).text("O");
+    console.log('third log', whosTurn);
+  }
+}
+
+//calling putXorO by clicking a grid
+$('.box').on('click', putXorO);
+
+
+
+// function assignX(){
+//   $(this).text("X");
+// }
+//
+// function assignO(){
+//   $(this).text("O");
+// }
+//
+// $('#a1').on('click', function() {
+//   console.log($(this).text());
+//   $(this).text("");
+// })
+
+function resetBoard() {
+  for (var i = resetGridArr.length - 1; i >= 0; i--) {
+    $eachGrid = $(resetGridArr[i]);
+    console.log($eachGrid);
+    $eachGrid.text("");
+  }
+}
+
+
+//Reset btn resets the grid to menpty
+$("#btn").on('click', function() {
   console.log($(this).text());
-  $(this).text("x");
+  $('.box').text("");
 })
-
-
-
-
-
 
 
 
