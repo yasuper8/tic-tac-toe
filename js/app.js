746fa8cbb9b66388aaeaf522d88f7df8b524e0a4
yasuper8 <ysuper8@gmail.com>
2016-10-28 20:15:22 -0700

    disabled reset btn when the page and activate after first click on a grid

--- js/app.js
+++ js/app.js
@@ -2,66 +2,50 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-console.log("app.js inked!")
+  console.log("app.js linked!")
+  $("#btn").prop("disabled",true);
+
+  var resetGridArr = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
 
 
-var resetGridArr = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
 
+  //keep track of player
+  var xTurn = true;
 
+  function isGridEmpy(){
 
-//keep track of player
-var xTurn = true;
-
-function isGridEmpy(){
-
-}
-
-//chaecks whos turn and place X or O
-function putXorO(whosTurn) {
-  if (xTurn && $(this).text("")) {
-    xTurn = false;
-    $(this).text("X");
-    // $(this).unbind('click');
-  } else if ($(this).text("")){
-    xTurn = true;
-    $(this).text("O");
-    // $(this).unbind('click');
-  ;
   }
-}
 
-//calling putXorO by clicking a grid
-$('.box').one('click', putXorO);
-
-
-
-// function assignX(){
-//   $(this).text("X");
-// }
-//
-// function assignO(){
-//   $(this).text("O");
-// }
-//
-// $('#a1').on('click', function() {
-//   console.log($(this).text());
-//   $(this).text("");
-// })
-
-function resetBoard() {
-  for (var i = resetGridArr.length - 1; i >= 0; i--) {
-    $eachGrid = $(resetGridArr[i]);
-    console.log($eachGrid);
-    $eachGrid.text("");
+  //chaecks whos turn and place X or O
+  function putXorO(whosTurn) {
+    $("#btn").prop("disabled",false);
+    if (xTurn && $(this).text("")) {
+      xTurn = false;
+      $(this).text("X");
+    } else if ($(this).text("")){
+      xTurn = true;
+      $(this).text("O");
+    }
   }
-}
 
-
-
-//Reset btn resets the grid to menpty
-$("#btn").on('click', function() {
-  console.log($(this).text());
-  $('.box').text("");
+  //calling putXorO by clicking a grid
   $('.box').one('click', putXorO);
+
+  function resetBoard() {
+    for (var i = resetGridArr.length - 1; i >= 0; i--) {
+      $eachGrid = $(resetGridArr[i]);
+      console.log($eachGrid);
+      $eachGrid.text("");
+    }
+  }
+
+  //Reset btn resets the grid to menpty
+  $("#btn").on('click', function() {
+    console.log($(this).text());
+    $('.box').text("");
+    $('.box').one('click', putXorO);
+    xTurn = true;
   })
-});
+
+
+}); //end .ready
