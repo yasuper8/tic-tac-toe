961158c22b7902b9bcf134291666e2ad4adb3941
yasuper8 <ysuper8@gmail.com>
2016-10-28 21:08:02 -0700

    fixed reset btn issue. Can reset the middle of a game

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
   console.log("app.js linked!")
-  $("#btn").prop("disabled",true);
+  // $("#btn").prop("disabled",true);
 
   var resetGridArr = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
 
@@ -12,38 +12,38 @@ $(document).ready(function() {
   //keep track of player
   var xTurn = true;
 
-  function isGridEmpy(){
+  function isRowMatch(){
 
   }
 
   //chaecks whos turn and place X or O
   function putXorO(whosTurn) {
-    $("#btn").prop("disabled",false);
+    // $("#btn").prop("disabled",false);
     if (xTurn && $(this).text("")) {
       xTurn = false;
       $(this).text("X");
+      $(this).css( 'pointer-events', 'none' );
     } else if ($(this).text("")){
       xTurn = true;
       $(this).text("O");
+      $(this).css( 'pointer-events', 'none' );
     }
   }
 
-  //calling putXorO by clicking a grid
-  $('.box').one('click', putXorO);
+  // Calling putXorO by clicking a grid
+  $('.box').on('click', putXorO);
 
-  function resetBoard() {
-    for (var i = resetGridArr.length - 1; i >= 0; i--) {
-      $eachGrid = $(resetGridArr[i]);
-      console.log($eachGrid);
-      $eachGrid.text("");
-    }
-  }
 
-  //Reset btn resets the grid to menpty
+  // clicking issues with .one and restting click by
+  // caling $('.box').on('click', putXorO); in reset cb
+  // $('.box').one('click', putXorO);
+
+
+  // Reset btn resets the grid to menpty
   $("#btn").on('click', function() {
     console.log($(this).text());
     $('.box').text("");
-    $('.box').one('click', putXorO);
+    $(".box").css( 'pointer-events', 'auto' ); // Making a block clickable again
     xTurn = true;
   })
 
